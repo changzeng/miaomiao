020dc7b99fabfedcb5ba6a0c14b093c1e4f40de8
changzeng <1207338829@qq.com>
2017-02-23 11:52:55 +0800

    optimize some calculation problems,add cut function

--- markov.py
+++ markov.py
@@ -9,27 +9,23 @@ class Markov:
         #--发射矩阵索引
         self.emit_index = {}
         with open("new_train.data") as fd:
-            #这个数字用来显示进度
-            num = 1
-            index = 0
-            while True:
-                print(num)
-                num += 1
-                char = fd.read(1)
-                #如果读到文件尾部
-                if char=='':
-                    break
-                try:
-                    self.emit_index[char]
-                except:
-                    self.emit_index[char] = index
-                    index += 1
+            self.str = fd.read()
+            
+        #这个数字用来显示索引和进度
+        index = 0
+        while self.str != '':
+            self.emit_index[self.str[0]] = index
+            self.str = self.str.replace(self.str[0],'')
+            index += 1
+            print("get character index "+str(index))
         
         #--汉字集合的大小
         self.emit_num = index
 
     #初始化markov模型的所有参数
     def initial_parameter(self):
+        #初始状态矩阵
+        self.pi = np.array([-1,0,0,-1])
         #如果存在文件
         if os.path.exists("markov.data"):
             with open("markov.data") as fd:
@@ -46,7 +42,8 @@ class Markov:
             #--状态索引
             self.state_num = 4
             #---状态数目
-            self.state_index = {'S':0,'M':1,'E':2,'S':3}
+            self.state_index = {'B':0,'M':1,'E':2,'S':3}
+            self.index_to_state = {0:'B',1:'M',2:'E',3:'S'}
             #--初始化发射矩阵
             self.emit_matrix = self.get_random_matrix(self.state_num,self.emit_num)
             #--状态转移矩阵
@@ -78,15 +75,13 @@ class Markov:
 
     #训练
     def train(self):
-        #初始状态矩阵
-        pi = np.array([-1,0,0,-1])
         #前序矩阵
         a = np.zeros((self.state_num,self.str_length),dtype=np.float64)
         #后序矩阵
         b = np.zeros((self.state_num,self.str_length),dtype=np.float64)
 
         #初始化前后向变量
-        a[:,0] = pi + self.emit_matrix[:,self.emit_index[self.str[0]]]
+        a[:,0] = self.pi + self.emit_matrix[:,self.emit_index[self.str[0]]]
         b[:,self.str_length-1] = 0
 
         progress = 1
@@ -97,14 +92,15 @@ class Markov:
             for j in range(self.state_num):
                 #计算向前变量
                 tmp = a[:,i-1]+self.transfer_matrix[j,:]
-                a[j,i] = tmp.max()+log2((2**(tmp-tmp.max())).sum())+self.get_emit_probability(j,i)
+                a[j,i] = self.add(tmp)+self.get_emit_probability(j,i)
                 #计算向后变量
                 tmp = b[:,self.str_length-i]+self.get_emit_probability(-1,i)+self.transfer_matrix[j,:]
-                b[j][self.str_length-i-1]=tmp.max()+log2((2**(tmp-tmp.max())).sum())
+                b[j][self.str_length-i-1] = self.add(tmp)
 
         #初始化条件矩阵概率
         progress = 1
         self.condition_matrix = np.zeros((self.str_length,self.state_num,self.state_num))
+        self.probability = np.zeros((self.state_num,self.str_length-1))
         #计算条件概率
         for i in range(self.str_length-1):
             print("calculate condition matrix "+str(progress)+"/"+str(self.str_length-1))
@@ -113,42 +109,31 @@ class Markov:
                 for k in range(self.state_num):
                     #待优化
                     self.condition_matrix[i,j,k] = a[j,i]+self.transfer_matrix[j,k]+self.get_emit_probability(k,i+1)+b[k,i+1]
+                self.probability[j,i] = self.add(self.condition_matrix[i,j,:])
+                
+        self.probability_sum = np.zeros((self.state_num,1))
+        for i in range(self.state_num):
+            print("calculate condition probability's summary "+str(i+1)+"/"+str(self.state_num))
+            self.probability_sum[i,0] = self.add(self.probability[i,:])
     
         #重新估计transfer_matrix和emit_matrix
         for i in range(self.state_num):
-            tmp = self.sate_probability(i)
-
             #重新估计transfer_matrix
             progress = 1
             for j in range(self.state_num):
                 print("calculate transfer matrix "+str(progress)+"/"+str(self.state_num))
-                self.transfer_matrix[i,j] = self.add(self.condition[:,i,j]) - tmp
+                self.transfer_matrix[i,j] = self.add(self.condition_matrix[:,i,j]) - self.probability_sum[i,0]
                 progress += 1
 
-            #重新估计emit_matrix
-            progress = 1
-            for key in slef.emit_index:
-                print("calculate emit matrix "+str(progress)+"/"+str(self.str_length))
-                key_index = self.emit_index[key]
-                self.emit_matrix[i,key_index] = self.add((self.str[:-1] == key)*self.probability[i,:]) - self.sate_probability(i)
-                progress += 1
-        
-    #计算处于状态i的概率
-    def sate_probability(self,index):
-        try:
-            self.probability
-        except:
-            self.probability = np.zeros((self.state_num,self.str_length-1))
-            self.probability_sum = np.zeros((self.state_num,1))
-            progress = 1
+        #重新估计emit_matrix
+        progress = 1
+        for key in self.emit_index:
+            print("calculate emit matrix "+str(progress)+"/"+str(self.emit_num))
+            key_index = self.emit_index[key]
+            bool_array = self.str[:-1] == key
+            progress += 1
             for i in range(self.state_num):
-                for j in range(self.str_length-1):
-                    print("calculate sum of probability "+str(progress)+"/"+str(self.state_num*(self.str_length-1)))
-                    self.probability[i,j] = self.add(self.condition_matrix[j,i,:])
-                    progress += 1
-            self.probability_sum[i,0] = self.add(self.probability[i,:])
-        
-        return self.probability_sum[index,0]
+                self.emit_matrix[i,key_index] = self.add((bool_array)*self.probability[i,:]) - self.probability_sum[i,0]
 
     #将ndarray中各元素相加
     def add(self,array):
@@ -169,6 +154,37 @@ class Markov:
             result.append(list(item))
         return result
 
+    #分词
+    def cut(self,string):
+        #维特比算法的临时数组
+        tmp = np.zeros((len(string),self.state_num))
+        #记录父节点
+        parents = np.zeros((len(string),self.state_num))
+        parents[0,:] = -1
+        tmp[0,:] = self.pi + self.emit_matrix[:,item_index]
+        #维特比算法向前
+        for i in range(len(string)-1):
+            for j in range(self.state_num):
+                former = tmp[i-1,:]+self.transfer_matrix[:,j]
+                tmp[i,j] = former.max()+self.emit_index[j,string[i]]
+                parents[i,j] = self.max_index(former)
+        #维特比算法向后
+        previous = self.max_index(tmp[-1,:])
+        state_chain = self.index_to_state[previous]
+        for i in list(range(1,len(string)))[::-1]:
+            previous = tmp[i,previous]
+            state_chain += self.index_to_state[previous]
+        state_chain = state_chain[::-1]
+
+    #返回最大值索引
+    def max_index(self,array):
+        max_value = array.max()
+        for i in range(len(array)):
+            if max_value == array[i]:
+                return i
+
+        return -1
+
     #将数据写入文件
     def save(self):
         markov = {}
@@ -183,5 +199,6 @@ class Markov:
         
 markov = Markov()
 markov.initial_parameter()
-markov.train()
-markov.save()
+while True:
+    markov.train()
+    markov.save()
